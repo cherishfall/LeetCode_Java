0bef05d9b867be17d9e8a7b86c84c3dd706a8d1b
cherishfall <cherishfall@hotmail.com>
2021-02-24 00:43:38 +0800

    环形链表

---
+++ src/HasCycle.java
@@ -0,0 +1,40 @@
+public class HasCycle {
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution_16 {
+    public boolean hasCycle(ListNode head) {
+        ListNode fastPtr = head, slowPtr = head;
+
+        while (fastPtr != null && fastPtr.next != null && slowPtr != null){
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+
+            if(slowPtr == fastPtr){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
