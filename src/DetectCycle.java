495332781cf8e065805ec76d8b16e7cc4a94aa91
cherishfall <cherishfall@hotmail.com>
2021-02-24 01:00:42 +0800

    环形链表II

---
+++ src/DetectCycle.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+public class DetectCycle {
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> nodes = new HashSet<>();
+
+        ListNode ptr = head;
+        while (ptr != null){
+            if(nodes.contains(ptr)){
+                return ptr;
+            }else {
+                nodes.add(ptr);
+            }
+
+            ptr = ptr.next;
+        }
+
+        return null;
+    }
+}
